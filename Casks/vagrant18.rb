a7195e0dd85c7c4507bf624d1f881d3afa07d487
Marko Samuli Kirves <marko.kirves@bynd.com>
2016-12-22 10:56:21 +0000

    Update Vagrant to v1.8.7

--- Casks/vagrant18.rb
+++ Casks/vagrant18.rb
@@ -1,6 +1,6 @@
 cask 'vagrant18' do
-  version '1.8.5'
-  sha256 'f12879c4890c39ec49af95153a6151b19fd137cbdccf7db22470f31464b9dd56'
+  version '1.8.7'
+  sha256 '14d68f599a620cf421838ed037f0a1c4467e1b67475deeff62330a21fda4937b'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}.dmg"
